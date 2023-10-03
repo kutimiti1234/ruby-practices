8e759196d0628f813d51383c543c1af72d744851
kutimiti1234 <tarbit.offensive@gmail.com>
2023-10-03 23:17:25 +0900

    最初の週が右詰めにならなかったので、修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,8 +1,20 @@
-require 'date'
-first_day = Date.new(2010,5,1)
-Last_day= Date.new(2010,5,-1)
+# frozen_string_literal: true
 
-Range.new(first_day,Last_day ).each do  |d|
-  puts " " if d.saturday?
-  print d.day.to_s.rjust(02) + ' '
+require 'date'
+first_day = Date.new(2023, 11, 1)
+Last_day = Date.new(2023, 11, -1)
+
+# 0が日曜日のハッシュをweekと定義する。
+week = { 0 => '', 1 => '', 2 => ' ', 3 => ' ', 4 => ' ', 5 => ' ', 6 => ' ' }
+Range.new(first_day, Last_day).each do |d|
+
+  week[d.wday] = d.day
+  next unless d.saturday? || d == Last_day
+
+  # 土曜日出ない限りweekに数字を入れる。
+  week.each_value do |key|
+    print "#{key.to_s.rjust(2)} "
+  end
+  puts ''
+  week = { 0 => '', 1 => '', 2 => ' ', 3 => ' ', 4 => ' ', 5 => ' ', 6 => ' ' }
 end
