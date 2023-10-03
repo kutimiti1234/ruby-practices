c839a50866fc6e8ba616db71422a06414347e251
kutimiti1234 <tarbit.offensive@gmail.com>
2023-10-03 21:04:17 +0900

    カレンダー部分の完成

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,1 +1,8 @@
-puts
+require 'date'
+first_day = Date.new(2010,5,1)
+Last_day= Date.new(2010,5,-1)
+
+Range.new(first_day,Last_day ).each do  |d|
+  puts " " if d.saturday?
+  print d.day.to_s.rjust(02) + ' '
+end
