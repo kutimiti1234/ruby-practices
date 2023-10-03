c3c3d70b240d967c6474e695d73ff5d1f4ce13b5
kutimiti1234 <tarbit.offensive@gmail.com>
2023-10-04 00:37:18 +0900

    今日の日付の部分の色が反転する(背景色と文字色が入れ替わる)

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,20 +1,28 @@
 # frozen_string_literal: true
 
 require 'date'
-first_day = Date.new(2023, 11, 1)
-Last_day = Date.new(2023, 11, -1)
+First_day = Date.new(2023, 10, 1)
+Last_day = Date.new(2023, 10, -1)
 
+title = "#{First_day.month.to_s}月 #{First_day.year.to_s}"
+puts title.center(20)
+puts '日 月 火 水 木 金 土'
 # 0が日曜日のハッシュをweekと定義する。
 week = { 0 => '', 1 => '', 2 => ' ', 3 => ' ', 4 => ' ', 5 => ' ', 6 => ' ' }
-Range.new(first_day, Last_day).each do |d|
-
-  week[d.wday] = d.day
+Range.new(First_day, Last_day).each do |d|
+  week[d.wday] = if d != Date.today
+                   d.day.to_s
+                 else
+                   "\e[7m#{d.day}\e[0m".to_s
+                 end
   next unless d.saturday? || d == Last_day
 
   # 土曜日出ない限りweekに数字を入れる。
-  week.each_value do |key|
-    print "#{key.to_s.rjust(2)} "
+  week.each_value do |value|
+    print "#{value.rjust(2)} "
   end
+  # 週の改行
   puts ''
+  # weekを初期化
   week = { 0 => '', 1 => '', 2 => ' ', 3 => ' ', 4 => ' ', 5 => ' ', 6 => ' ' }
 end
