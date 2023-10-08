0b910b160a04be46a5453d2c6ee810581a8cb46a
kutimiti1234 <tarbit.offensive@gmail.com>
2023-10-08 16:06:50 +0900

    Revert "同じ処理を条件分岐していたので統合"
    This reverts commit 99c117e208692b65673f771a48c86f8326f9138d.

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,10 +1,8 @@
+# !/usr/bin/env ruby
 # frozen_string_literal: true
 
-
-# !/usr/bin/env ruby
 score = ARGV[0]
 scores = score.split(',')
-
 shots = []
 scores.each do |s|
   if s == 'X'
@@ -17,31 +15,42 @@ end
 
 frames = []
 # ありえない投球数、数字、記号が来ない前提
-# 10フレーム目
 shots.each_slice(2) do |s|
   frames << s
 end
 
 point = 0
 frames.each_with_index do |frame, index|
-  # 10フレーム目だけ例外的な処理として行う。
-  next unless index < 10
+  # 10投目だけ別の処理として行う。
+  if index < 9
+    point += frame.sum
 
-  point += frame.sum
+    if frame[0] == 10 # strike
+      point += if frames[index.succ][0] != 10
+                 frames[index.succ][0] + frames[index.succ][1]
+               else
+                 10 + frames[index.succ.succ][0]
+               end
+    elsif frame.sum == 10 # spare
+      point += frames[index.succ][0]
+    end
+  elsif index == 9
+    point += frame.sum
+    if frame[0] == 10
+      point += if frames[index.succ][0] == 10
+                 frames[index.succ].sum + frames[index.succ.succ][0]
+               else
+                 frames[index.succ].sum
+               end
+    elsif frame.sum == 10
+      point += frames[index.succ][0]
+    end
 
-  # ストライクの場合、あとの2投を点数に含むよう条件を設定
-  if frame[0] == 10 # strike
-    # ストライクが2連続で発生した場合,indexの2個後ろのフレームも参照できるよう処理
-    point += if frames[index.succ][0] == 10
-               10 + frames[index.succ.succ][0]
-             # ストライクが2連続で発生しなかった場合、index1個後ろのフレームの一投目と2投目を代入
-             else
-               frames[index.succ][0] + frames[index.succ][1]
-             end
-  # スペアの場合、indexの1個後ろのフレームの1投目を代入
-  elsif frame.sum == 10 # spare
-    point += frames[index.succ][0]
   end
-
 end
 puts point
+
+# 1.10投目で３つ数字が入る可能性
+# 2.配列の次の数字をどのように手に入れるか。
+# 10投目はframes.sizでif文を作る。
+#
