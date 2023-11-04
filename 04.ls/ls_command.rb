0c893524e3c7e3071dd507700cea7f9dc2880798
kutimiti1234 <tarbit.offensive@gmail.com>
2023-11-04 18:39:55 +0900

    入力に関する項目を完成

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -1,18 +1,25 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def file_names_formatter(file_names, display_max_line)
+require 'optparse'
+
+def determine_files_order(file_names, display_max_line)
   # 最大表示幅を考慮に入れながら列ごとの表示するファイルの位置を決定する。
-  (file_names.to_a.size / display_max_line..file_names.to_a.size).each do |x|
+  min_slice_number = if file_names.to_a.size / display_max_line < 1
+                       1
+                     else
+                       file_names.to_a.size / display_max_line
+                     end
+  (min_slice_number..file_names.to_a.size).each do |x|
     display_file_names = file_names.each_slice(x).to_a
     return display_file_names if file_names.each_slice(x).to_a.size <= display_max_line
   end
 end
 
-def file_names_make_displayable(display_file_names)
+def convert_to_displayable_array(display_file_names)
   # directory_displayで転地するために、転値可能な行列に編集する。
   display_file_names.last.fill('', display_file_names.last.size...display_file_names.first.size)
-  # 列ごとに最大文字数似合わせて幅を作る。
+  # 列ごとに最大文字数に合わせて幅を作る。
   display_file_names.map! do |inner_array|
     max_words = inner_array.max_by(1, &:size).first.size
     inner_array.map! do |element|
@@ -22,7 +29,7 @@ def file_names_make_displayable(display_file_names)
 end
 
 def directory_display(display_file_names_displayable)
-  # 要件の表示順に合わせて転地させたものを一つづつ表示する。
+  # 要件の表示順に合わせて転置させたものを一つづつ表示する。
   display_file_names_displayable.transpose.each_with_index do |cols, _row|
     cols.each_with_index do |cell, _col|
       print "#{cell}  "
@@ -31,13 +38,12 @@ def directory_display(display_file_names_displayable)
   end
 end
 
-a = Dir.foreach('.').sort
-display_max_line = 15
+files = Dir.glob('*', base: ARGV[0]).sort
 
-b = file_names_formatter(a, display_max_line)
+display_max_line = 3
 
-p file_names_make_displayable(b)
+ordered_files = determine_files_order(files, display_max_line)
 
-p b
+convert_to_displayable_array(ordered_files)
 
-directory_display(b)
+directory_display(ordered_files)
