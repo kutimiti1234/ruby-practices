5497ac79b7d610f180bcdfd59288166261fb85d5
kutimiti1234 <tarbit.offensive@gmail.com>
2024-07-07 02:07:51 +0900

    集計行を追加

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -15,28 +15,34 @@ def main
     wc_info << [:bytesize, input_text.size] if argv_options[:c]
     output_text << wc_info.to_h
   end
-
+  output_text << file_total_info(output_text)
   max_width = get_max_width(output_text)
   print_output(output_text, max_width)
 end
 
 DISPLAY_ADJUST_NUMBER = 1
+TOTAL = '合計'
+
+def file_total_info(output_text)
+  file_total_info = output_text.flat_map(&:to_a).group_by(&:first).reject { |k, _v| k == :filename }
+  file_total_info.transform_values { |value| value.map(&:last).sum }.merge(filename: TOTAL)
+end
 
 def print_output(output_text, max_width)
   output_text.each do |output_line|
-    long_sentence = "#{output_line[:line].to_s.rjust(max_width[:line_display_witdh]) || ''}" \
-                    "#{output_line[:word].to_s.rjust(max_width[:word_display_width]+DISPLAY_ADJUST_NUMBER) || ''}" \
-                    "#{output_line[:bytesize].to_s.rjust(max_width[:bytesize_display_width]+DISPLAY_ADJUST_NUMBER) || ''}" \
+    long_sentence = "#{output_line[:line].to_s.rjust(max_width[:line_display_width]) || ''}" \
+                    "#{output_line[:word].to_s.rjust(max_width[:word_display_width]) || ''}" \
+                    "#{output_line[:bytesize].to_s.rjust(max_width[:bytesize_display_width] ).concat(' ') || ''}" \
                     "#{output_line[:filename]}"
     puts long_sentence
   end
 end
 
 def get_max_width(output_text)
-  max_lines_width = output_text.map { |entry| entry[:line].to_s.length }.max || 0
-  max_words_width = output_text.map { |entry| entry[:word].to_s.length }.max || 0
+  max_lines_width = output_text.map { |entry| entry[:line].to_s.length }.max + DISPLAY_ADJUST_NUMBER || 0
+  max_words_width = output_text.map { |entry| entry[:word].to_s.length }.max + DISPLAY_ADJUST_NUMBER || 0
   max_bytesizes_width = output_text.map { |entry| entry[:bytesize].to_s.length }.max || 0
-  { line_display_witdh: max_lines_width, word_display_width: max_words_width, bytesize_display_width: max_bytesizes_width }
+  { line_display_width: max_lines_width, word_display_width: max_words_width, bytesize_display_width: max_bytesizes_width }
 end
 
 def parse_and_remove_options(argv)
