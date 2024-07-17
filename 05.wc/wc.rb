955cd88afa18f78c2eabfecc2a48ec33cb8ee4bb
kutimiti1234 <tarbit.offensive@gmail.com>
2024-07-17 22:56:35 +0900

    変数名と関数名をわかりやすく。ロジックも読みやすくなるように修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,56 +3,58 @@
 
 require 'optparse'
 
-SHOW_ADJUST_SPACE_SIZE = 1
-STANDARD_INPUT_EXCEPTIONAL_WORD = '-'
+SEPARATOR_SIZE = 1
+STDIN_FILENAME = '-'
 TOTAL = '合計'
 
 def main
   options = parse_options(ARGV)
   counts = []
-  ARGF.each(nil) do |standard_input_or_file|
+  ARGF.each(nil) do |input_text|
     count = {}
 
     count[:filename] = ARGF.filename
-    count[:line] = standard_input_or_file.lines.count if options[:l]
-    count[:word] = standard_input_or_file.split(/\s+/).size if options[:w]
-    count[:bytesize] = standard_input_or_file.size if options[:c]
+    count[:line] = input_text.lines.count if options[:l]
+    count[:word] = input_text.split(/\s+/).size if options[:w]
+    count[:bytesize] = input_text.size if options[:c]
     counts << count
   end
-  rows = counts
-  # 複数行出力する場合は、集計行を表示する
-  rows << get_wc_total_row(counts) if counts.size > 1
-  max_column_widths = get_max_column_widths(rows)
-  show_wc_rows(rows, max_column_widths)
+  counts << calculate_counts_total(counts) if counts.size > 1
+  max_column_widths = calculate_wc_max_column_widths(counts)
+  show_wc_rows(counts, max_column_widths)
 end
 
-def get_wc_total_row(counts)
-  total_row = counts.inject({}) do |result, column_name|
-    result.merge(column_name) { |_key, current_val, adding_value| current_val + adding_value }
+def calculate_counts_total(counts)
+  counts_without_filename = counts.map { |count| count.except(:filename) }
+
+  total = counts_without_filename.inject({}) do |result, count|
+    result.merge(count) do |_key, current_val, adding_value|
+      current_val + adding_value
+    end
   end
-  total_row[:filename] = TOTAL
-  total_row
+  total[:filename] = TOTAL
+  total
 end
 
 def show_wc_rows(rows, max_column_widths)
   rows.each do |row|
     cells = {}
-    row.each_pair do |column_name, cell|
+    row.each do |column_name, cell|
       cells[column_name] = if column_name == :filename
-                             # 標準入力を受け取る際に例外的に"-"が入力されるため除外する
-                             cell unless cell == STANDARD_INPUT_EXCEPTIONAL_WORD
+                             cell unless cell == STDIN_FILENAME
                            else
-                             cell.to_s.rjust(max_column_widths[column_name] + SHOW_ADJUST_SPACE_SIZE)
+                             column_width = max_column_widths[column_name] + SEPARATOR_SIZE
+                             cell.to_s.rjust(column_width)
                            end
     end
     puts "#{cells[:line]}#{cells[:word]}#{cells[:bytesize]} #{cells[:filename]}"
   end
 end
 
-def get_max_column_widths(wc_rows)
-  max_lines_width = wc_rows.map { |entry| entry[:line].to_s.length  }.max
-  max_words_width = wc_rows.map { |entry| entry[:word].to_s.length  }.max
-  max_bytesizes_width = wc_rows.map { |entry| entry[:bytesize].to_s.length }.max
+def calculate_wc_max_column_widths(rows)
+  max_lines_width = rows.map { |entry| entry[:line].to_s.length }.max
+  max_words_width = rows.map { |entry| entry[:word].to_s.length }.max
+  max_bytesizes_width = rows.map { |entry| entry[:bytesize].to_s.length }.max
   { line: max_lines_width, word: max_words_width, bytesize: max_bytesizes_width }
 end
 
