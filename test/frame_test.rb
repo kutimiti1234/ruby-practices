f1e49f37b7e3af77ad91ab4a25d8ef94a77815b4
kutimiti1234 <tarbit.offensive@gmail.com>
2024-08-05 01:03:51 +0900

    clean:rubocop

--- test/frame_test.rb
+++ test/frame_test.rb
@@ -19,12 +19,13 @@ class FrameTest < Minitest::Test
     expected_score = 10
     assert_equal expected_score, frame.score
   end
+
   def test_frame_score_of_three_shots
     first_shot_score = 'X'
     second_shot_score = 'X'
     third_shot_score =  'X'
     expected_score = 30
-    frame = Frame.new(first_shot_score, second_shot_score,third_shot_score)
+    frame = Frame.new(first_shot_score, second_shot_score, third_shot_score)
     assert_equal expected_score, frame.score
   end
 end
