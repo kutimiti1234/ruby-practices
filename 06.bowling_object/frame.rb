825625bcab824a929531a50737ed5c853559935f
kutimiti1234 <tarbit.offensive@gmail.com>
2024-08-05 12:22:44 +0900

    clean:frameのスコア計算をframeクラスへ移管

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -5,14 +5,22 @@ require_relative 'shot'
 class Frame
   attr_reader :first_shot, :second_shot, :third_shot
 
-  def initialize(first_mark, second_mark = nil, third_mark = nil)
+  def initialize(game, first_mark, second_mark = nil, third_mark = nil)
+    @game = game
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
     @third_shot = Shot.new(third_mark)
   end
 
   def score
-    [@first_shot, @second_shot, @third_shot].map(&:score).sum
+    basic = [@first_shot, @second_shot, @third_shot].map(&:score).sum
+    if strike?
+      basic + strike_bonus
+    elsif spare?
+      basic + spare_bonus
+    else
+      basic
+    end
   end
 
   def strike?
@@ -23,5 +31,25 @@ class Frame
     return false if strike?
 
     [@first_shot, @second_shot].map(&:score).sum == 10
+  end
+
+  private
+
+  def strike_bonus
+    index = @game.frames.find_index(self)
+    return 0 if index == 9
+
+    if !@game.frames[index + 1].second_shot.exist?
+      [@game.frames[index + 1].first_shot, @game.frames[index + 2].first_shot].map(&:score).sum
+    else
+      [@game.frames[index + 1].first_shot, @game.frames[index + 1].second_shot].map(&:score).sum
+    end
+  end
+
+  def spare_bonus
+    index = @game.frames.find_index(self)
+    return 0 if index == 9
+
+    @game.frames[index + 1].first_shot.score
   end
 end
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -6,12 +6,14 @@ require_relative 'frame'
 STRIKE = 'X'
 
 class Game
+  attr_reader :frames
+
   def initialize
     @frames = parse_marks
   end
 
   def score
-    calculate_basic_score + calculate_bonus_score
+    @frames.map(&:score).sum
   end
 
   private
@@ -22,38 +24,13 @@ class Game
     9.times do
       rolls = pins.shift(2)
       if rolls.first == STRIKE
-        frames << Frame.new(rolls.first)
+        frames << Frame.new(self, rolls.first)
         pins.unshift rolls.last
       else
-        frames << Frame.new(*rolls)
+        frames << Frame.new(self, *rolls)
       end
     end
 
-    frames << Frame.new(*pins)
-  end
-
-  def calculate_basic_score
-    @frames.map(&:score).sum
-  end
-
-  def calculate_bonus_score
-    bonus = 0
-    9.times do |index|
-      bonus += calculate_strike_bonus(index) if @frames[index].strike?
-      bonus += calculate_spare_bonus(index) if @frames[index].spare?
-    end
-    bonus
-  end
-
-  def calculate_strike_bonus(index)
-    if !@frames[index + 1].second_shot.exist?
-      [@frames[index + 1].first_shot, @frames[index + 2].first_shot].map(&:score).sum
-    else
-      [@frames[index + 1].first_shot, @frames[index + 1].second_shot].map(&:score).sum
-    end
-  end
-
-  def calculate_spare_bonus(index)
-    @frames[index + 1].first_shot.score
+    frames << Frame.new(self, *pins)
   end
 end
