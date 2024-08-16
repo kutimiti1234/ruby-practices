c900f9f63ebce23353057611aa503157f2b4e264
kutimiti1234 <tarbit.offensive@gmail.com>
2024-08-16 22:15:02 +0900

    clean:不要な条件を削除

--- 07.ls_object/ls_long.rb
+++ 07.ls_object/ls_long.rb
@@ -41,7 +41,7 @@ class LsLong
 
   def render_directories
     @dir_entries.each do |dir_entry|
-      puts if dir_entry.path.directory? && (@dir_entries.size >= 2 || !@file_entries.empty?)
+      puts if @dir_entries.size >= 2 || !@file_entries.empty?
       file_max_sizes = find_max_sizes(dir_entry.file_entries)
       render(dir_entry, file_max_sizes)
     end
