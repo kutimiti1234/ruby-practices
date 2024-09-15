7b9ea539ef6d7ad246969a0d522f5110ef571384
kutimiti1234 <tarbit.offensive@gmail.com>
2024-09-15 21:00:46 +0900

    clean:DirEntryクラスからフォーマット機能を排除

--- 07.ls_object/dir_entry.rb
+++ 07.ls_object/dir_entry.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'file_entry'
-require_relative 'entries'
 
 class DirEntry
-  include Entries
-
   attr_accessor :path, :file_entries
 
   def initialize(path, options)
@@ -14,23 +11,14 @@ class DirEntry
     @file_entries = collect_file_entries
   end
 
-  def run_ls_short(width)
-    max_file_name_count = @file_entries.map { |f| f.stats[:name].size }.max
-    col_count = width / (max_file_name_count + 1)
-    row_count = col_count.zero? ? @file_entries.count : (@file_entries.count.to_f / col_count).ceil
-    transposed_file_entries = safe_transpose(@file_entries.each_slice(row_count).to_a)
-    format_table(transposed_file_entries, max_file_name_count)
-  end
-
-  def run_ls_long
-    header = ["合計 #{total}"]
-    max_sizes = find_max_sizes
-    body = @file_entries.map { |entry| entry.run_ls_long(max_sizes) }.join("\n")
-    [header, body].join("\n")
+  def find_max_sizes
+    %i[nlink user group size].to_h do |key|
+      [key, @file_entries.map { |entry| entry.send(key).size }.max]
+    end
   end
 
   def total
-    @file_entries.map(&:stats).sum { |data| data[:blocks] }.to_i
+    @file_entries.map(&:blocks).sum
   end
 
   private
@@ -38,7 +26,7 @@ class DirEntry
   def collect_file_entries
     pattern = @path.join('*')
     params = @options[:dot_match] ? [pattern, File::FNM_DOTMATCH] : [pattern]
-    file_entries = Dir.glob(*params).map { |file| FileEntry.new(file, called_from_dir: true) }.sort_by { |file| file.stats[:name] }
+    file_entries = Dir.glob(*params).map { |file| FileEntry.new(file) }.sort_by(&:name)
     @options[:reverse] ? file_entries.reverse : file_entries
   end
 end
